c063b35c41c1458d8c57cabe5e8f17cc21834a13
salik-dev <bahadurali0346556@gmail.com>
2024-10-29 22:02:23 +0500

    bahadur

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", 'http://127.0.0.1:3000'],
     credentials: true,
   })
 );
